03e209f8bdda1ecc33699a3c351e54e8ea0b6985
israeloyeyemi28 <israeloyeyem28@gmail.com>
2024-01-09 17:34:02 +0100

    uyy

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,101);
+        int guess = -1;
+        while (guess != magicNumber)
+        {
+            Console.Write("Whats your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher!!!");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower!!!");
+            }
+            else
+            {
+                Console.WriteLine("You got it");
+            }
+        }
     }
 }
