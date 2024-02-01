5f92c1ef93693e62270a565469db8b05c593a892
Israel Oyeyemi <135351226+israeloyeyemi28@users.noreply.github.com>
2024-02-01 12:34:05 +0100

    Update Program.cs

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,69 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        // This will clear the console
+        Console.Clear();
+        Console.Write("\n*** Welcome to the Mindfulness Program ****\n");
+        //Call Choices
+        Choices choice = new Choices();
+        int seconds;
+
+        int action = 0;
+        while (action != 4)
+        {
+            // Ask for user input (1-4)
+            action = choice.UserChoice();
+            switch (action)
+            {
+                case 1:
+                    // Start Breathing Activity
+                    Console.Clear();
+                    BreathingActivity breathing = new BreathingActivity("Breathing", 0);
+                    breathing.GetActivityName();
+                    breathing.GetActivityDescription();
+                    seconds = breathing.GetActivityTime();
+                    breathing.GetReady();
+                    breathing.Breathing(seconds);
+                    breathing.GetDone();
+                    break;
+                case 2:
+                    //Start Reflecting Activity
+                    Console.Clear();
+                    ReflectingActivity reflecting = new ReflectingActivity("Reflecting", 0);
+                    reflecting.GetActivityName();
+                    reflecting.GetActivityDescription();
+                    seconds = reflecting.GetActivityTime();
+                    reflecting.GetReady();
+                    reflecting.ShowPrompt(seconds);
+                    reflecting.GetDone();
+                    break;
+                case 3:
+                    //Start Listing Activity
+                    Console.Clear();
+                    ListingActivity listing = new ListingActivity("Listing", 0);
+                    listing.GetActivityName();
+                    listing.GetActivityDescription();
+                    seconds = listing.GetActivityTime();
+                    listing.GetReady();
+                    listing.ReturnPrompt(seconds);
+                    listing.GetDone();
+                    break;
+                case 4:
+                    // Quite
+                    Console.WriteLine("\nThank you for using the Mindfulness Program!\n");
+                    break;
+                default:
+                    Console.WriteLine($"\nSorry the option you entered is not valid.");
+                    break;
+            }
+        }
+
+
     }
+
 }
